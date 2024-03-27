e83a7f39610f45c0101fb5a3a3ef355ea82bd19a
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-03-26 22:30:29 -0230

    Added butterfly obstacle and scoreboard

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-//Canvas
+//Canvas//
 let board;
 let boardWidth = 980;
 let boardHeight = 730;
 let context;
 
-//Sprit Character
+//Sprit character//
 let spritWidth = 75;
 let spritHeight = 75;
 let spritX = 75
@@ -18,21 +18,29 @@ let sprit = {
     height : spritHeight
 }
 
-//Plant Obstacle//
+//Plant obstacle//
 let plantWidth = 120;
 let plantHeight = 260;
-let plantX = 150
-let plantY = 500
+let plantX = 150;
+let plantY = 500;
 let plantImg;
 
-//Game Operations//
+//Butterfly obstacle//
+let butterflyWidth = 115;
+let butterflyHeight = 115;
+let butterflyX = 150;
+let butterflyY = 500;
+let butterflyImg;
+
+//Game operations//
 let velocityX = 0;
 let velocityY = 0;
 let gravity = 0;
+
 let gameOver = false;
 let score = 0;
 
-//Initial Load - Draw Background and Sprit (Possibly animate so it looks as if it is floating)//
+// When the screen initial loads it will draw  the background and sprit (Possibly animate so it looks as if it is floating)//
 window.onload = function () {
     board = document.getElementById("board");
     board.height = boardHeight;
@@ -42,15 +50,25 @@ window.onload = function () {
     spritImg = new Image();
     spritImg.src = "./Images/Sprit.png";
     spritImg.onload = function() {
-    context.drawImage(spritImg, sprit.x, sprit.y, sprit.width, sprit.height); // Pulling infomation from Sprit Character//
+    context.drawImage(spritImg, sprit.x, sprit.y, sprit.width, sprit.height); // This pulls the infomation from the section Sprit Character//
     }
+
 //Spawn Plant Obstacle//
     plantImg = new Image();
     plantImg.scr = "./Images/Plant.png";
 
+    butterflyImg = new Image();
+    butterflyImg.scr = "./Images/Butterfly.png"
+
     requestAnimationFrame(update);
     setInterval(placePlant, 1000)
-    document.addEventListener("keydown". moveSprit);
-}
+    document.addEventListener("keydown". moveSprit);}
 
-
+    function update() {
+    //Score//
+    context.fillStyle="black";
+    context.font="25px courier";
+    score++;
+    context.fillText(score, 15, 30);
+    }
+   
