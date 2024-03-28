343d83bf5333eafc931357316f583add693a9a20
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-03-28 16:06:18 -0230

    Added character mouvement and plant obstacle to JAVA

--- script.js
+++ script.js
@@ -1,60 +1,61 @@
+//Game objects//
 var spiritCharacter;
-
-function startGame (){
-    spiritCharacter = new component(30, 30, "Spirit.png", 10, 120, "image");
-    gameArea.start();
-}
-
+var plantObstacle;
 var gameArea = {
-    canvas: document.createElement("canvas"),
-    start: function () {
+    canvas : document.createElement("canvas"),
+    start : function() {
         this.canvas.width = 980;
         this.canvas.height = 730;
-        this.context = 
-        this .canvas.getContext("2d");
+        this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.frameNo = 0;
-        this.interval = setInterval(updateGame, 20);
-        },
-        clear : function() {
-            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        },
-        stop: function () {
-            clearInterval(this.interval);
-        } 
+        this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+            gameArea.keys = (gameArea.keys || []);
+            gameArea.keys[e.keyCode] = (e.type == "keydown");
+        })
+        window.addEventListener('keyup', function (e) {
+            gameArea.keys[e.keyCode] = (e.type == "keydown");            
+        })
+    }, 
+    clear : function(){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
+}
+//Start game//
+function startGame() {
+    gameArea.start();
+    spiritCharacter = new component(75, 75, "lightgray", 100, 500); //Controls the size and location of spirit character//
+    plantObstacle = new component(120, 260, "green", 350, 450); //Controls the size and location of plant obstacle//
+}
+//This adds components to the game area//
+function component(width, height, color, x, y) {
+    this.gamearea = gameArea;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        ctx = gameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }    
+}
 
-    function component(width, height, color, x, y, type) {
-        this.type = type;
-        if (type == "image") {
-            this.image = new Image();
-            this.image.scr = color;
-        }
-        this.width = width;
-        this.height = height;
-        this.speedX = 0;
-        this.speedY - 0;
-        this.x = x;
-        this.y = y;
-        this.update = function() {
-            ctx = gameArea.context;
-            if (type == "image") {
-                ctx.drawImage(this.image, 
-                    this.x,
-                    this.y,
-                    this.width, this.height);
-            } else {
-                ctx.fillStyle = color;
-                ctx.fillRect(this.x, this.y, this.width, this.height);
-            }
-        }
-        this.newPos = function (){
-            this.x += this.speedX;
-            this.y += this.speedY;
-        }
-    }
-    function updateGameArea(){
-        gameArea.clear();
-        spiritCharacter.newPos();
-        spiritCharacter.update();
-    }
+function updateGameArea() {
+    gameArea.clear();
+    spiritCharacter.speedX = 0;
+    spiritCharacter.speedY = 0;
+    if (gameArea.keys && gameArea.keys[37]) {spiritCharacter.speedX = -1; }
+    if (gameArea.keys && gameArea.keys[39]) {spiritCharacter.speedX = 1; }
+    if (gameArea.keys && gameArea.keys[38]) {spiritCharacter.speedY = -1; }
+    if (gameArea.keys && gameArea.keys[40]) {spiritCharacter.speedY = 1; }
+    plantObstacle.update();
+    spiritCharacter.newPos();
+    spiritCharacter.update();
+  }
