d1540a9012ce08989cb53e3884b6f7f00cbf7948
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-03-29 10:40:34 -0230

    Added new HTML index to test a game menu

--- script.js
+++ script.js
@@ -13,63 +13,99 @@ let spiritX = 75;
 let spiritY = 500;
 
 //Spirit object//
-let sprit = {
+let spirit = {
     x : spiritX,
     y : spiritY,
     width : spiritWidth,
     height : spiritHeight
 } 
 
-//Plant obstacle//
-let plantImg;
-let plantWidth = 120;
-let plantHeight = 260;
-let plantX = 150;
-let plantY = 500;
+//Obstacles//
+let obstacleArray = [];
 
-//Butterfly obstacle//
-let butterflyImg;
-let butterflyWidth = 115;
-let butterflyHeight = 115;
-let butterflyX = 150;
-let butterflyY = 500;
+let obstacle1Width = 120;
+let obstacle2Width = 115; 
+
+let obstacleHeight = 260;
+let obstacleX = 700;
+let obstacleY = boardHeight - obstacleHeight;
+
+let obstacle1Img;
+let obstacle2Img;
 
 //Game Physics and Operations//
-let velocityX = 0;
+let velocityX = -8;
 let velocityY = 0;
-let gravity = 0;
+let gravity = .4;
 let gameOver = false;
 let score = 0;
 
 //Game Functions//
-// When Screen Loads// *Possibly animate the spirit so it floating
+// When Screen Loads// *Possibly animate the spirit so it floating or use multi button movement contorls
 window.onload = function () {
     board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
     context = board.getContext("2d"); //This draws the game background//
-    
-    spritImg = new Image();
-    spritImg.src = "./Images/Spirit.png";
-    spritImg.onload = function() {
-    context.drawImage(spritImg, sprit.x, sprit.y, sprit.width, sprit.height); // This draws the spirit character//
+
+//Draws spirit character//
+    spiritImg = new Image();
+    spiritImg.src = "./Images/Spirit.png";
+    spiritImg.onload = function() {
+    context.drawImage(spiritImg, spirit.x, spirit.y, spirit.width, spirit.height); // This draws the spirit character//
     }
 
-//Plant Obstacle//
-    plantImg = new Image();
-    plantImg.scr = "./Images/Plant.png";
+    //Obstacles//
+    obstacle1Img = new Image();
+    obstacle1Img.scr = "./Images/Plant.png";
 
-    butterflyImg = new Image();
-    butterflyImg.scr = "./Images/Butterfly.png"
+    obstacle2Img = new Image();
+    obstacle2Img.scr = "./Images/Butterfly.png"
 
     requestAnimationFrame(update);
     setInterval(placePlant, 1000)
-    document.addEventListener("keydown". moveSprit);} //This will tell the browser that you want to perform an animation//
-
+    document.addEventListener("keydown". moveSpirit); //This will tell the browser that you want to perform an animation//
+}
     function update() {
-    //Score//
-    context.fillStyle="black";
-    context.font="25px courier";
-    score++;
-    context.fillText(score, 15, 30);
+        if (gameOver) {
+            return;
+        }
+        context.clearRect(0, 0, board.width, board.height); //This clears the canvas//
+       
+        //Spirit Character
+        velocityY += gravity;
+        spirit.y = Math.min(spirit.y + velocityY, spiritY); //apply gravity to current spirit.y so it doesn't exceed the ground//
+        context.drawImage(spiritImg, spirit.x, spirit.y, spirit.width, spirit.height);
+    
+        //Obstacles//
+        for (let i = 0; i < obstacleArray.length; i++) {
+            let obstacle = obstacleArray[i];
+            obstacle.x += velocityX;
+            context.drawImage(obstacle.img, obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+    
+            if (detectCollision(spirit, obstacle)) {
+                gameOver = true;
+            }
+        }
+        //score
+        context.fillStyle="black";
+        context.font="20px courier";
+        score++;
+        context.fillText(score, 5, 20);
     }
+
+    
+function moveSpirit(e) {
+    if (gameOver) {
+        return;
+    }
+
+    if ((e.code == "Space" || e.code == "ArrowUp") && spirit.y == spiritY) {
+        //jump
+        velocityY = -10;
+    }
+    else if (e.code == "ArrowDown" && spirit.y == spiritY) {
+        //duck
+    }
+
+}
